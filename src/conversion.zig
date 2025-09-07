167f94f179a563dd3635673c7e7e984382738a3a
shimekukuri <jhutchinson192390@gmail.com>
2025-09-06 20:50:32 -0400

    f

--- src/conversion.zig
+++ src/conversion.zig
@@ -1,11 +1,13 @@
 //! This module provides functionality for casting to and from vector types
 const std = @import("std");
+const testing = std.testing;
 const suggestVectorLength = std.simd.suggestVectorLength;
 
-///For castin from a Vec to a slice of given T. Usually used in with sliceAsVec to easily move back and forth between
-///the two types while operating on the same backing data. The vector length is automatically Selected as by std.simd.suggestVectorLength
-///It is recomended that you use whatever length is generated from suggestVectorLength as it is going to be targeting
-///whatever cpu features are available at compile time, and the flags that were passed into the build command.
+///For casting from a Vec to a slice of given T where T is aligned to alignement of @alignOf(@Vector(suggestVectorLength(T) orelse 1, T)).
+///Usually used in with sliceAsVec to easily move back and forth between the two types while operating on the same backing data.
+///The vector length is automatically Selected as by std.simd.suggestVectorLength It is recomended that you use whatever length
+///is generated from suggestVectorLength as it is going to be targeting whatever cpu features are available at compile time, and
+///the flags that were passed into the build command.
 pub inline fn vecAsSlice(comptime T: type, vec: *@Vector(suggestVectorLength(T) orelse 1, T)) *const [suggestVectorLength(T) orelse 1]T {
     return @ptrCast(vec);
 }
@@ -14,7 +16,7 @@ pub inline fn vecAsSlice(comptime T: type, vec: *@Vector(suggestVectorLength(T) 
 ///the two types while operating on the same backing data. The vector length is automatically Selected as by std.simd.suggestVectorLength
 ///It is recomended that you use whatever length is generated from suggestVectorLength as it is going to be targeting
 ///whatever cpu features are available at compile time, and the flags that were passed into the build command.
-pub inline fn sliceAsVec(comptime T: type, slice: []T) *@Vector(suggestVectorLength(T) orelse 1, T) {
+pub inline fn sliceAsVec(comptime T: type, slice: []align(@alignOf(@Vector(suggestVectorLength(T) orelse 1, T))) T) *@Vector(suggestVectorLength(T) orelse 1, T) {
     return @ptrCast(@alignCast(slice));
 }
 
@@ -22,13 +24,10 @@ test "sliceAsVecSuceeds" {
     var exampleArr align(@alignOf(@Vector(suggestVectorLength(u8) orelse 1, u8))) = [_]u8{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3 };
     const exampleSlice = exampleArr[0..];
     const x = sliceAsVec(u8, exampleSlice);
-    try std.testing.expect(@TypeOf(x) == *@Vector(suggestVectorLength(u8) orelse 1, u8));
+    try testing.expect(@TypeOf(x) == *@Vector(suggestVectorLength(u8) orelse 1, u8));
 }
 
-//const VT = @Vector(std.simd.suggestVectorLength(u8), u8);
-//pub inline fn sliceAsVec(slice: []u8) *VT {
-//    return @ptrCast(@alignCast(slice));
-//}
+test "sliceAsVecFailsWrongAlignment" {}
 
 pub fn generateOptimizedVectorType(comptime T: type) type {
     const N = std.simd.suggestVectorLength(T);
