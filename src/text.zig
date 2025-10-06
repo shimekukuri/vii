2650af48e54575228458294fec55e45f116c2143
HutchinsoJ <hutchinsoj@Cmhmaclap43459.local>
2025-10-06 09:06:53 -0400

    updated core version

--- src/text.zig
+++ src/text.zig
@@ -1,8 +1,7 @@
 const conversion = @import("conversion.zig");
 const VectorAlignedSlice = conversion.VectorAlignedSlice;
 
-pub fn findIndex(comptime T: type, slice: VectorAlignedSlice(T)) usize {
-    //todo
+pub inline fn findIndex(comptime T: type, slice: VectorAlignedSlice(T)) usize {
     _ = slice;
     return 0;
 }
